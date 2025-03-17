c8bb9eec3c0d55e3db176074f0c69176311929a8
sharmavaibhav31 <sharma31stmay@gmail.com>
2025-03-18 00:47:22 +0530

    Camera test with model calling

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.air_notepad"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
